e86e11c12064f338323aa2c3d93c43d41f26bb51
tejasva255 <68278445+tejasva255@users.noreply.github.com>
2021-01-15 19:52:27 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,14 @@
+class Game {
+	constructor(){
+  
+	}
+	loading(){
+
+	}
+	instruction(){
+		text("welcome to earn to live",400,800)
+		var playButton = createSprite(500,500,50,50) 
+		if (mousePressedOver(playButton)) {
+		}
+	}
+}
