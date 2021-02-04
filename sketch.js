e55e7ff44af0a6077344770ef840a80be77c434b
tejasva255 <68278445+tejasva255@users.noreply.github.com>
2021-02-04 19:26:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 
-var gameState,game;
+var gameState="loading";
+var game;
+var loadingPng
 
+function preload(){
+loadingPng = loadImage("images/download.png")
+}
 function setup() {
+  
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-  gameState="instruction"
   game= new Game()
 }
+
+var playButton;
 
 function draw() {
   background(255,255,255);  
