bfa07abbae918f99a6b850d907580911daf83cc0
tejasva255 <68278445+tejasva255@users.noreply.github.com>
2021-02-11 20:22:45 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -6,18 +6,29 @@ class Game {
 		text("loading page",400,200)
 	 playButton = createSprite(450,210,50,50)
 	 playButton.addImage(loadingPng)
-		drawSprites();
+		
 		if (mousePressedOver(playButton)) {
-        gameState="instruction";
+       gameState="instruction";
         playButton.visible=false;
+	}
+	else{
+		playButton.visible=true;
 	}
 }
 	instruction(){
 		text("welcome to earn to live",400,200)
+		playButton2 = createSprite(487,309,50,50)
+        if(mousePressedOver(playButton2)){
+ 	    gameState="play"
+         }
 		playButton.visible=false;
 		}
 	
 	play(){
+    boy = createSprite(100,300,10,10)
+    ground = createSprite(400,370,800,40)
+    ground.velocityX=-4
+    ground.x=ground.width/2 
 
 	}
 
