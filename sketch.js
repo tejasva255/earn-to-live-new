653ae586c23e8d7ad8cd4a0a0fe7e90ac034352c
tejasva255 <68278445+tejasva255@users.noreply.github.com>
2021-02-11 20:22:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 
 var gameState="loading";
 var game;
-var loadingPng
+var loadingPng,groundPng;
 
 function preload(){
 loadingPng = loadImage("images/download.png")
+groundPng = loadImage("images/stage-1.png")
 }
 function setup() {
   
@@ -13,14 +14,29 @@ function setup() {
 }
 
 var playButton;
+var playButton2;
+var boy,ground;
+
 
 function draw() {
   background(255,255,255);  
   drawSprites();
+  text(mouseX+","+mouseY,mouseX,mouseY)
+
    if (gameState==="loading") {
    	game.loading();
    }
    else if (gameState==="instruction") {
    	game.instruction();
+   }
+
+   else if (gameState==="play") {
+    game.play();
+   }
+   function keyPressed() {
+    if (keyCode===32) {
+      boy.velocityY = -10
+    }
+     // body...
    }
 }
